e7c1648f81f306de983b9375626cb1c794a3803d
Jeremy Mann <jeremy.mann@corp.alkamitech.com>
2020-12-11 11:28:29 -0600

    Day1 Done

--- Day11/advent2020-day11.py
+++ Day11/advent2020-day11.py
@@ -7,61 +7,88 @@ def Part1(matrix):
         counter += 1
         for i, line in enumerate(matrix):
             for j, entry in enumerate(matrix[i]):
-                if entry == "L":
-                    if i == 0:
-                        if j == 0:
-                            testlist = [matrix[i][j+1], matrix[i+1][j], matrix[i+1][j+1]]
+                if i == 0:
+                    if j == 0:
+                        testlist = [matrix[i][j+1], matrix[i+1][j], matrix[i+1][j+1]]
+                        if entry == "L":
                             if '#' not in testlist:
                                 newmatrix[i][j] = '#'
-                        elif j < len(matrix[i]) - 1:
-                            testlist = [matrix[i][j-1], matrix[i+1][j-1], matrix[i+1][j], matrix[i+1][j+1], matrix[i][j+1]]
+                    elif j < len(matrix[i]) - 1:
+                        testlist = [matrix[i][j-1], matrix[i+1][j-1], matrix[i+1][j], matrix[i+1][j+1], matrix[i][j+1]]
+                        if entry == "L":
                             if '#' not in testlist:
                                 newmatrix[i][j] = '#'
-                        else:
-                            testlist = [matrix[i][j-1], matrix[i+1][j-1], matrix[i+1][j]]
-                            if '#' not in testlist:
-                                newmatrix[i][j] = '#'
-                    elif i < len(matrix) - 1:
-                        if j == 0:
-                            testlist = [matrix[i-1][j], matrix[i-1][j+1], matrix[i][j+1], matrix[i+1][j+1], matrix[i+1][j]]
-                            if '#' not in testlist:
-                                newmatrix[i][j] = '#'
-                        elif j < len(matrix[i]) - 1:
-                            testlist = [matrix[i-1][j-1], matrix[i-1][j], matrix[i-1][j+1], matrix[i][j+1], matrix[i+1][j+1], matrix[i+1][j], matrix[i+1][j-1], matrix[i][j-1]]
-                            if '#' not in testlist:
-                                newmatrix[i][j] = '#'
-                        else:
-                            testlist = [matrix[i-1][j-1], matrix[i-1][j], matrix[i+1][j], matrix[i+1][j-1], matrix[i][j-1]]
-                            if '#' not in testlist:
-                                newmatrix[i][j] = '#'
+                        elif entry == '#':
+                            if testlist.count('#') >= 4:
+                                newmatrix[i][j] = "L"
                     else:
-                        if j == 0:
-                            testlist = [matrix[i-1][j], matrix[i-1][j+1], matrix[i][j+1]]
+                        testlist = [matrix[i][j-1], matrix[i+1][j-1], matrix[i+1][j]]
+                        if entry == "L":
                             if '#' not in testlist:
                                 newmatrix[i][j] = '#'
-                        elif j < len(matrix[i]) - 1:
-                            testlist = [matrix[i-1][j-1], matrix[i-1][j], matrix[i-1][j+1], matrix[i][j+1], matrix[i][j-1]]
+                elif i < len(matrix) - 1:
+                    if j == 0:
+                        testlist = [matrix[i-1][j], matrix[i-1][j+1], matrix[i][j+1], matrix[i+1][j+1], matrix[i+1][j]]
+                        if entry == "L":
                             if '#' not in testlist:
                                 newmatrix[i][j] = '#'
-                        else:
-                            testlist = [matrix[i-1][j-1], matrix[i-1][j], matrix[i][j-1]]
+                        elif entry == '#':
+                            if testlist.count('#') >= 4:
+                                newmatrix[i][j] = "L"
+                    elif j < len(matrix[i]) - 1:
+                        testlist = [matrix[i][j-1], matrix[i+1][j-1], matrix[i+1][j], matrix[i+1][j+1], matrix[i][j+1], matrix[i-1][j+1], matrix[i-1][j], matrix[i-1][j-1]]
+                        if entry == "L":
                             if '#' not in testlist:
                                 newmatrix[i][j] = '#'
-        print(counter, newmatrix)
-        break
+                        elif entry == '#':
+                            if testlist.count('#') >= 4:
+                                newmatrix[i][j] = "L"
+                    else:
+                        testlist = [matrix[i][j-1], matrix[i+1][j-1], matrix[i+1][j], matrix[i-1][j], matrix[i-1][j-1]]
+                        if entry == "L":
+                            if '#' not in testlist:
+                                newmatrix[i][j] = '#'
+                        elif entry == '#':
+                            if testlist.count('#') >= 4:
+                                newmatrix[i][j] = "L"
+                else:
+                    if j == 0:
+                        testlist = [matrix[i-1][j], matrix[i-1][j-1], matrix[i][j-1]]
+                        if entry == "L":
+                            if '#' not in testlist:
+                                newmatrix[i][j] = '#'
+                    elif j < len(matrix[i]) - 1:
+                        testlist = [matrix[i][j-1], matrix[i-1][j-1], matrix[i-1][j], matrix[i-1][j+1], matrix[i][j+1]]
+                        if entry == "L":
+                            if '#' not in testlist:
+                                newmatrix[i][j] = '#'
+                        elif entry == '#':
+                            if testlist.count('#') >= 4:
+                                newmatrix[i][j] = "L"
+                    else:
+                        testlist = [matrix[i][j-1], matrix[i-1][j-1], matrix[i-1][j]]
+                        if entry == "L":
+                            if '#' not in testlist:
+                                newmatrix[i][j] = '#'
+                    
 
-with open(r"Day11\day11-input-small.txt") as file:
+        print(counter)
+
+        if newmatrix == matrix:
+            seatcount = 0
+            for entry in matrix:
+                seatcount += entry.count('#')
+            print(seatcount)
+            break
+        matrix = deepcopy(newmatrix)
+
+with open(r"Day11\day11-input.txt") as file:
     lines = [x.strip() for x in file.readlines()]
 
 matrix = []
 for i, line in enumerate(lines):
     matrix.append(list(line))
 
-#for i, entry in enumerate(matrix):
-#    for j, char in enumerate(entry):
-#        print('({i},{j}),{char}'.format(i = i, j = j, char = char), end = ' ')
-#    print('')
-
 Part1(matrix)
-#print(matrix)
+
 
